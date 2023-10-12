67b25e3be2e62004bdee3904adf6911f2e54828b
10Hyuna <kimha105@naver.com>
2023-10-12 16:29:14 +0900

    [add] API 멸세서

---
+++ src/main/java/com/example/ecotag/EcotagApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecotag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcotagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecotag/EcotagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecotag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcotagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
